7c58009b5d429e508037eb9c6c127f56ffb25507
anthonyiu <yoyoanthonyiu@gmail.com>
2022-05-04 11:45:52 +0100

    2nd backup

--- js/ramen.js
+++ js/ramen.js
@@ -1,54 +1,14 @@
+import { words } from "../js/words.js";
+
 let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const keyboard = document.getElementById("keyboard");
 const message = document.getElementById("message");
-const passwordBoard = [
-  "Aeroplane",
-  "Dinosaur",
-  "Fire Engine",
-  "Tank",
-  "Zoo",
-  "Truck",
-  "Rocket",
-  "Igloo",
-  "Book",
-  "Queen",
-  "Tomato",
-  "Chips",
-  "Food",
-  "Plate",
-  "Pigeon",
-  "Snake",
-  "Bird",
-  "Pig",
-  "Oven",
-  "Milk",
-  "Orange",
-  "Purple",
-  "Rabbit",
-  "Moon",
-  "Sun",
-  "Father",
-  "Mother",
-  "Door",
-  "Window",
-  "Bread",
-  "Shoes",
-  "Mouth",
-  "Mouse",
-  "King",
-  "Flower",
-  "Planet",
-  "Arrow",
-  "Money",
-  "Summer",
-  "Winter",
-  "Spring",
-];
+const timer = document.querySelector("#timer");
 const passwordDiv = document.querySelector("#board");
-const imgDiv = document.querySelector("#buppyRamen");
-const coins = document.querySelector("#coin-remain");
-const random = Math.floor(Math.random() * passwordBoard.length);
-const password = passwordBoard[random];
+
+const coins = document.querySelector("#coins");
+const random = Math.floor(Math.random() * words.length);
+const password = words[random];
 let fail = 0;
 let countDown;
 const start = function () {
@@ -57,7 +17,8 @@ const start = function () {
     keyboard.insertAdjacentHTML("beforeend", html);
   });
   showPassword();
-  showHangman(fail);
+  showEyes(fail);
+  showCoins(fail);
 };
 window.onload = start;
 const passwordDashed = password.split("").map((letter) => {
@@ -66,24 +27,34 @@ const passwordDashed = password.split("").map((letter) => {
   else if (letter === ",") return ",";
   else return "_";
 });
+
+const showCoins = (e) => {
+  coins.innerHTML = `C x ${6 - e}`;
+
+  if (6 - e == 1) {
+    coins.classList.add("lastcoin");
+  }
+};
+
+const deductFood = (e) => {
+  document.querySelector(`#food${e}`).style.display = "none";
+};
 const showPassword = function () {
   passwordDiv.innerHTML = passwordDashed.join("");
 };
-const showHangman = function (nr) {
-  imgDiv.innerHTML = `<img src="img/${nr}.png" alt="" />`;
-  if (nr) {
-    imgDiv.classList.add("shake");
+const showEyes = function (e) {
+  if (e > 0) {
+    document.querySelector(`#eyes${e - 1}`).style.display = "none";
   }
-  window.setTimeout(function () {
-    imgDiv.classList.remove("shake");
-  }, 300);
-  // const stopshake = function () {};
+  if (e == "won") {
+    document.querySelector(`#eyes${fail - 1}`).style.display = "none";
+  }
+  document.querySelector(`#eyes${e}`).style.display = "block";
 };
 
 const checkForLetter = function (e) {
   if (e.target.classList.contains("letter")) {
     if (password.toUpperCase().split("").includes(e.target.textContent)) {
-      //   yes.play();
       password
         .toUpperCase()
         .split("")
@@ -95,10 +66,18 @@ const checkForLetter = function (e) {
         });
       deactivateLetter(true, e.target);
     } else {
-      //   no.play();
       fail++;
-      showHangman(fail);
-      coins.innerHTML = 6 - fail;
+      showEyes(fail);
+      deductFood(fail);
+      showCoins(fail);
+
+      if (fail) {
+        coins.classList.add("shake");
+      }
+      window.setTimeout(function () {
+        coins.classList.remove("shake");
+      }, 300);
+
       deactivateLetter(false, e.target);
     }
     if (fail == 6) {
@@ -116,21 +95,26 @@ const deactivateLetter = function (hit, letter, audio) {
 const finish = function (success) {
   if (success) {
     message.innerHTML = `<h1 class="won">WELL DONE!</h1><a class='btn'><i class="fa-solid fa-arrow-rotate-right"></i> PLAY AGAIN</a>`;
+
     passwordDiv.classList.add("won");
-    imgDiv.classList.add("won");
+    keyboard.classList.add("won");
+    showEyes("won");
+
     clearInterval(countDown);
   } else {
     message.innerHTML = `<h1 class="lost">YOU LOST!</h1>
     <div class="answer">The answer is: <br><span class="password">${password}</span></div><a class='btn'><i class="fa-solid fa-arrow-rotate-right"></i> TRY AGAIN</a>`;
+
+    keyboard.classList.add("lost");
     passwordDiv.classList.add("lost");
+
     clearInterval(countDown);
   }
   document
     .querySelector("a.btn")
     .addEventListener("click", () => location.reload());
 };
-const timer = function () {
-  const timer = document.querySelector("#timer");
+const timerCount = function () {
   let time = new Date(300000);
   const options = {
     minute: "2-digit",
@@ -145,11 +129,13 @@ const timer = function () {
     if (time == 0) {
       finish(false);
       clearInterval(countDown);
-      showHangman(6);
+
+      // all eyes off but 6 on, all food off
+      showEyes(6);
     }
   };
   tick();
   countDown = setInterval(tick, 1000);
   return countDown;
 };
-timer();
+timerCount();
